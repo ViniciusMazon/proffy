b158ed985212b080e5442fc894d573bfa4cc17a5
Vinicius Mazon <vnpmazon@gmail.com>
2020-08-03 17:50:02 -0300

    feat(web): Adds Landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Landing from './pages/Landing';
 import './assets/styles/global.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Let's hack!</h1>
-    </div>
+    <Landing />
   );
 }
 
