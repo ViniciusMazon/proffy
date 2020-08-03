33a0b19b3659e3a12115d36af27154fa793f2b2a
Vinicius Mazon <vnpmazon@gmail.com>
2020-08-03 17:12:34 -0300

    chore(web): Adds global styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './assets/styles/global.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Let's hack!</h1>
+      <h1>Let's hack!</h1>
     </div>
   );
 }
