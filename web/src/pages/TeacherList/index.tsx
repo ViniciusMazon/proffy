bc5b27c63b463e19c72b88d426349a1aad6dfb18
Vinicius Mazon <vnpmazon@gmail.com>
2020-08-03 22:12:50 -0300

    feat(web): Adds TeacherItem to the TeacherList page

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 import './styles.css';
 
 function TeacherList() {
@@ -24,6 +25,12 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </main>
     </div>
   );
 }
