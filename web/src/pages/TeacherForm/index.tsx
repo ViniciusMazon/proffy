cc11a91651d12bc63d95ba911edd6dd599ea662c
Vinicius Mazon <vnpmazon@gmail.com>
2020-08-03 21:40:58 -0300

    feat(web): Adds header with form to the TeacherList page

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
 
 function TeacherForm() {
-  return <h1>Teacher Form</h1>
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Teste" />
+    </div>
+  );
 }
 
 export default TeacherForm;
