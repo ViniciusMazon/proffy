58f1902a5d27a5b5af95889b23f6b198355afb07
Vinicius Mazon <vnpmazon@gmail.com>
2020-08-05 20:13:41 -0300

    feat(web): Adds prop description to PageHeader component

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -7,6 +7,7 @@ import './styles.css';
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -21,6 +22,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
       <div className="header-content">
         <strong>{props.title}</strong>
+        {props.description && <p>{props.description}</p>}
         {props.children}
       </div>
     </header >
