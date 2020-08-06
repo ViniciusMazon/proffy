0b9ac64ef4c2e0ab1927163560d638e0f37b489c
Vinicius Mazon <vnpmazon@gmail.com>
2020-08-06 18:08:35 -0300

    chore(mobile): Adds project base

---
+++ mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+
+import styles from './styles';
+
+import landingImg from '../../assets/images/landing.png';
+
+function Landing() {
+  return (
+    <View style={styles.container}>
+      <Image source={landingImg} style={styles.banner} />
+      <Text style={styles.title}>
+        Seja bem-vindo, {'\n'}
+        <Text style={styles.titleBold}>O que deseja fazer?</Text>
+      </Text>
+    </View>
+  );
+}
+
+export default Landing;
---
+++ mobile/src/pages/Landing/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#8257E5',
+    padding: 40,
+  },
+  banner: {
+    width: '100%',
+    resizeMode: 'contain',
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 20,
+    lineHeight: 30,
+    marginTop: 80,
+  },
+  titleBold: {
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
