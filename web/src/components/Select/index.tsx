8d00ff04123acef3c207f640a255c2a74bf00242
Vinicius Mazon <vnpmazon@gmail.com>
2020-08-05 22:10:37 -0300

    feat(web): Adds axios and the api component

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -14,8 +14,8 @@ const Select: React.FC<SelectProps> = ({ label, name, options, ...rest }) => {
   return (
     <div className="select-block">
       <label htmlFor={name}>{label}</label>
-      <select id={name} {...rest}>
-        <option value="" disabled selected hidden>Selecione uma opção</option>
+      <select defaultValue="" id={name} {...rest}>
+        <option value="" disabled hidden>Selecione uma opção</option>
 
         {options.map(option => {
           return <option
