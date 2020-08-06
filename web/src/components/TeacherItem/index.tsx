3b00fedb948f84c28189484409be20c7696b161c
Vinicius Mazon <vnpmazon@gmail.com>
2020-08-05 23:59:17 -0300

    feat(web): Integrates the TeacherItem component with the api

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,34 +1,51 @@
 import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 import './styles.css';
 
-const TeacherItem = () => {
+
+export interface Teacher {
+  id: number;
+  subject: string;
+  cost: number;
+  name: string;
+  avatar: string;
+  whatsapp: string;
+  bio: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function createNewConnection() {
+    api.post('/connections', { user_id: teacher.id });
+  }
+
   return (
     <article className="teacher-item">
       <header>
         <img
-          src="https://avatars3.githubusercontent.com/u/38103866?s=460&u=244951efa29035b28d90d168c50cd497cde3b9d5&v=4"
-          alt="Vinicius Mazon"
+          src={teacher.avatar}
+          alt={teacher.name}
         />
         <div>
-          <strong>Vinicius Mazon</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
       <p>
-        Entusiasta das melhores tecnologias de química avançada.
-      <br /><br />
-      Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-    </p>
+        {teacher.bio}
+      </p>
       <footer>
         <p>
           Preço/hora
-        <strong>R$: 80,00</strong>
+        <strong>R$: {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a target="_blank" href={`https://wa.me${teacher.whatsapp}`} onClick={createNewConnection} >
           <img src={whatsappIcon} alt="Whatsapp" />
         Entrar em contato
-      </button>
+      </a>
       </footer>
     </article>
   );
