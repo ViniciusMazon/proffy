cc9adb1d1e24c1344c5e1db7c995b34b4a9b9e98
Vinicius Mazon <vnpmazon@gmail.com>
2020-08-07 16:06:52 -0300

    feat(mobile): Adds API integration

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ScrollView } from 'react-native';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { useFocusEffect } from '@react-navigation/native'
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import styles from './styles';
 
 function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+        setFavorites(favoritedTeachers);
+      }
+    });
+  }
+
+  useFocusEffect(() => {
+    loadFavorites();
+  });
+
   return (
     <View style={styles.container}>
-    <PageHeader title="Meus proffys favoritos"/>
-    <ScrollView
+      <PageHeader title="Meus proffys favoritos" />
+      <ScrollView
         style={styles.teacherList}
         contentContainerStyle={{
           paddingHorizontal: 16,
           paddingBottom: 16
         }}
       >
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {favorites.map((teacher: Teacher) => {
+          return (
+            <TeacherItem
+              key={teacher.id}
+              teacher={teacher}
+              favorited
+            />
+          )
+        })}
       </ScrollView>
-  </View>
+    </View>
   );
 }
 
