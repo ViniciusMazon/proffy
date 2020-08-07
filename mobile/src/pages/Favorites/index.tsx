08703eb1bffdd95b4b0c14e5dab4faa8fc456d72
Vinicius Mazon <vnpmazon@gmail.com>
2020-08-07 11:42:39 -0300

    feat(mobile): Adds Favorites page

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { ScrollView } from 'react-native';
 import { Text, View } from 'react-native';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 import styles from './styles';
 
 function Favorites() {
   return (
     <View style={styles.container}>
     <PageHeader title="Meus proffys favoritos"/>
+    <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
   </View>
   );
 }
