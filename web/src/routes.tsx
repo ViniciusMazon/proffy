83d9d4ee0f1cc967136681d551acd6def1d13349
Vinicius Mazon <vnpmazon@gmail.com>
2020-08-10 09:39:11 -0300

    chore(web): Adds route to login page

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import Login from './pages/Login';
 import Landing from './pages/Landing';
 import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
@@ -8,7 +9,8 @@ import TeacherForm from './pages/TeacherForm';
 function Routes() {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={Landing} />
+      <Route exact path="/" component={Login} />
+      <Route exact path="/home" component={Landing} />
       <Route path="/study" component={TeacherList} />
       <Route path="/give-classes" component={TeacherForm} />
     </BrowserRouter>
