1986d28093fd58ecb6b702b5337621e46c534ca5
Vinicius Mazon <vnpmazon@gmail.com>
2020-08-10 15:34:48 -0300

    feat(web): Base of login page

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
 
+import heartPurple from '../../assets/images/icons/purple-heart.svg';
+
 import './styles.css';
 
 function Login() {
   return (
-    <h1>Login</h1>
+    <div id="page-login">
+      <div id="logo-container" />
+
+      <main className="form-container">
+        <form>
+          <h1>Fazer login</h1>
+          <input className="custom-input" id="custom-input-top" type="email" placeholder="E -mail" />
+          <input className="custom-input" id="custom-input-bottom" type="password" placeholder="Senha" />
+
+          <div className="remember-and-forget">
+            <span>
+              <input type="checkbox" />
+              <label>Lembrar-me</label>
+            </span>
+            <a href="#">Esqueci minha senha</a>
+          </div>
+
+          <button>Entrar</button>
+        </form>
+      </main>
+
+      <footer>
+        <p>
+          Não tem conta? <br />
+          <a href="#">Cadastre-se</a>
+        </p>
+        <p>É de graça
+            <img src={heartPurple} alt="Coração" />
+        </p>
+      </footer>
+    </div>
   );
 }
 
