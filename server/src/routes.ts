33fc0a0f3a9576d446da65e7feee3cc5bffeab16
Vinicius Mazon <vnpmazon@gmail.com>
2020-08-21 14:53:16 -0300

    feat(server): Add auth middleware

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -10,9 +10,9 @@ routes.post('/session', sessionController.create);
 routes.post('/registration', usersController.create);
 routes.post('/forgot-password', sessionController.forgotPassword);
 routes.post('/reset-password', sessionController.resetPassword);
-routes.post('/classes', classesController.store);
-routes.get('/classes', classesController.index);
-routes.post('/connections', connectionsController.store);
-routes.get('/connections', connectionsController.index);
+routes.post('/classes', sessionController.authenticate, classesController.store);
+routes.get('/classes', sessionController.authenticate, classesController.index);
+routes.post('/connections', sessionController.authenticate, connectionsController.store);
+routes.get('/connections', sessionController.authenticate, connectionsController.index);
 
 export default routes;
