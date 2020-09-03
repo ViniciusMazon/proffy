ecd98956b3c3fec4a03dd31f5d600be32c45d3d0
Vinicius Mazon <vnpmazon@gmail.com>
2020-09-03 15:29:36 -0300

    feat(web): Changes PageHeader style

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
+import rocketIcon from '../../assets/images/icons/rocket.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 import './styles.css';
 
 interface PageHeaderProps {
+  name?: string;
   title: string;
   description?: string;
+  icon?: string;
+  iconDescription?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -17,12 +21,19 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
         <Link to="/home">
           <img src={backIcon} alt="Voltar" />
         </Link>
+        <p>{props.name}</p>
         <img src={logoImg} alt="Logo Proffy" />
       </div>
 
       <div className="header-content">
         <strong>{props.title}</strong>
-        {props.description && <p>{props.description}</p>}
+        <div>
+          {props.description && <p>{props.description}</p>}
+          <span>
+            <img src={props.icon} alt={props.description} />
+            <p>{props.iconDescription}</p>
+          </span>
+        </div>
         {props.children}
       </div>
     </header >
