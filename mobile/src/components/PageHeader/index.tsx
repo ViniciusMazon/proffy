11a6dd91836420e3b3deb7b7209c1fcce2918367
Vinicius Mazon <vnpmazon@gmail.com>
2020-09-07 20:28:37 -0300

    feat(mobile): Changes the PageHeader component

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -9,11 +9,13 @@ import backIcon from '../../assets/images/icons/back.png';
 import logoImg from '../../assets/images/logo.png';
 
 interface PageHeaderProps {
+  name?: string;
   title: string;
+  subTitle?: string;
   headerRight?: ReactNode;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title, headerRight, children }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ name, title, subTitle, headerRight, children }) => {
   const { navigate } = useNavigation();
 
   function handleGoBack() {
@@ -26,12 +28,13 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title, headerRight, children })
         <BorderlessButton onPress={handleGoBack}>
           <Image source={backIcon} resizeMode="contain" />
         </BorderlessButton>
-
+        <Text style={styles.topBarName}>{name}</Text>
         <Image source={logoImg} resizeMode="contain" />
       </View>
 
       <View style={styles.header}>
         <Text style={styles.title}>{title}</Text>
+        <Text style={styles.subTitle}>{subTitle}</Text>
         {headerRight}
       </View>
 
--- mobile/src/components/PageHeader/styles.ts
+++ mobile/src/components/PageHeader/styles.ts
@@ -10,18 +10,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
   },
+  topBarName: {
+    fontFamily: 'Archivo_400Regular',
+    fontSize: 14,
+    lineHeight: 15,
+    color: '#D4C2FF',
+  },
   header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+
   },
   title: {
     fontFamily: 'Archivo_700Bold',
     color: '#FFF',
     fontSize: 24,
     lineHeight: 32,
-    maxWidth: 160,
-    marginVertical: 40,
+    marginTop: 41,
+    marginBottom: 16,
+  },
+  subTitle: {
+    fontFamily: 'Poppins_400Regular',
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#D4C2FF',
+    marginBottom: 32,
   },
 });
 
