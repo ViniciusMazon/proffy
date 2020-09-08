606bf75e40aeca33ab4783a487e14838cd9f81ec
Vinicius Mazon <vnpmazon@gmail.com>
2020-09-08 18:18:43 -0300

    feat(mobile): Change the Favorite page

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -6,10 +6,33 @@ import { useFocusEffect } from '@react-navigation/native'
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem, { Teacher } from '../../components/TeacherItem';
+
+import favoriteIcon from '../../assets/images/icons/favorite.png';
 import styles from './styles';
 
 function Favorites() {
-  const [favorites, setFavorites] = useState([]);
+  const [favorites, setFavorites] = useState([
+    {
+      id: 1,
+      subject: 'Inglês',
+      cost: '20',
+      name: 'Vinicius',
+      surname: 'Mazon',
+      avatar: 'https://github.com/viniciusmazon.png',
+      whatsapp: '77 99999999',
+      bio: 'Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.',
+      schedule: [{
+        weekDay: 'Segunda',
+        from: '8',
+        to: '18',
+      },
+      {
+        weekDay: 'Quarta',
+        from: '8',
+        to: '18',
+      }]
+    }
+  ]);
 
   function loadFavorites() {
     AsyncStorage.getItem('favorites').then(response => {
@@ -26,7 +49,12 @@ function Favorites() {
 
   return (
     <View style={styles.container}>
-      <PageHeader title="Meus proffys favoritos" />
+      <PageHeader
+      name="Estudar"
+      title="Meus proffys Favoritos"
+      icon={favoriteIcon}
+      iconText="1 proffy"
+      />
       <ScrollView
         style={styles.teacherList}
         contentContainerStyle={{
